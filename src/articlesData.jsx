3752c68d2ed92915d8a6e278b44fe280f56581ae
marianIR <marianraileanu37@gmail.com>
2023-06-10 20:12:06 +0300

    feat: new images update

--- src/articlesData.jsx
+++ src/articlesData.jsx
@@ -5,21 +5,21 @@ import maskGroupThree from "/src/images/Mask Group (2).png";
 const articlesData = [
   {
     id: 1,
-    illustration: { maskGroupOne },
+    illustration: maskGroupOne,
     title: "Disease detection, check up in the laboratory",
     content:
       "In this case, the role of the health laboratory is very important to doa disease detection...",
   },
   {
     id: 2,
-    illustration: { maskGroupTwo },
+    illustration: maskGroupTwo,
     title: "Herbal medicines that are safe for consumption",
     content:
       "Herbal medicine is very widely used at this time because of its very good for your health...",
   },
   {
     id: 3,
-    illustration: { maskGroupThree },
+    illustration: maskGroupThree,
     title: "Natural care for healthy facial skin",
     content:
       "A healthy lifestyle should start from now and also for your skin health.There are some...",
--- src/servicesData.jsx
+++ src/servicesData.jsx
@@ -7,42 +7,42 @@ import group from "/src/images/Group.png";
 const serviceData = [
   {
     id: 1,
-    image: { frameOne },
+    image: frameOne,
     category: "Search doctor",
     description:
       "Choose your doctor from thousands of specialist, general, and trusted hospitals",
   },
   {
     id: 2,
-    image: { frameTwo },
+    image: frameTwo,
     category: "Online pharmacy",
     description:
       "Buy  your medicines with our mobile application with a simple delivery system",
   },
   {
     id: 3,
-    image: { frameThree },
+    image: frameThree,
     category: "Consultation",
     description:
       "Free consultation with our trusted doctors and get the best recomendations",
   },
   {
     id: 4,
-    image: { frameFour },
+    image: frameFour,
     category: "Details info",
     description:
       "Free consultation with our trusted doctors and get the best recomendations",
   },
   {
     id: 5,
-    image: { frameFive },
+    image: frameFive,
     category: "Emergency care",
     description:
       "You can get 24/7 urgent care for yourself or your children and your lovely family",
   },
   {
     id: 6,
-    image: { group },
+    image: group,
     category: "Tracking",
     description: "Track and save your medical history and health data ",
   },
